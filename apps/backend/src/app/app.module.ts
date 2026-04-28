446a3cea00c33d08f6debe2352545fc0ff866780
Khas <khasduc@gmail.com>
2026-04-28 18:17:10 +1000

    feat: install pg, dotenv, drizzle-kit, zod, betterauth + init rest endpoints + modules

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { CandidatesModule } from './candidates/candidates.module';
+import { EmployersModule } from './employers/employers.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    CandidatesModule,
+    EmployersModule,
+    JobsModule,
+    ApplicationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
