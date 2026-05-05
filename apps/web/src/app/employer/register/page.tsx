edad6e4240fd1a5092946df2c7ff322a3e7df893
Pal1206 <phuonganhle05.work@gmail.com>
2026-05-05 18:07:37 +1000

    Create page.tsx

---
+++ apps/web/src/app/employer/register/page.tsx
@@ -0,0 +1,7 @@
+import { EmployerRegisterPage } from '../../../features/employer-register/employer-register-page';
+
+export const metadata = { title: 'Employer Register | TalentMatch' };
+
+export default function Page() {
+  return <EmployerRegisterPage />;
+}
