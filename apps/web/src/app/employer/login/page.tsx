0735fbeee3819051755db18269b19e912c7668b5
Pal1206 <phuonganhle05.work@gmail.com>
2026-05-05 18:07:04 +1000

    Create page.tsx

---
+++ apps/web/src/app/employer/login/page.tsx
@@ -0,0 +1,7 @@
+import { EmployerLoginPage } from '../../../features/employer-login/employer-login-page';
+
+export const metadata = { title: 'Employer Sign In | TalentMatch' };
+
+export default function Page() {
+  return <EmployerLoginPage />;
+}
